948baa36d08ffd4b7c118c621d91ecdd56c3f984
Felix Cagatin <cagatingilbert@gmail.com>
2022-08-08 13:38:17 -0700

    Added seed data for role table

--- seed/seed.sql
+++ seed/seed.sql
@@ -1,11 +1,24 @@
-INSERT into department
+INSERT INTO department
     (name)
 VALUES
     ("Human Resources"),
     ("IT"),
     ( "Accounting"),
     ("Finance"),
-    ( "Marketing"),
-    ("R&D"),
-    ( "Production")
-;
+    ( "Marketing");
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ("Human Resource Specialist", 68000, 1),
+    ("Human Resource Assistant", 45000, 1),
+    ("Network Engineer", 120000, 2),
+    ("IT Technician", 52000, 2),
+    ("Senior Accountant", 95000, 3),
+    ("Tax Manager", 95000, 3),
+    ("Controller", 150000, 4),
+    ("Financial Manager", 120000, 4),
+    ("Chief Marketing Officer", 90000, 5),
+    ("Brand Manager", 98000, 5);
+
+    
