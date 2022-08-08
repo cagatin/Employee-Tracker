d774fd5df6676a8688afa5d8817a42148d1d571c
Felix Cagatin <cagatingilbert@gmail.com>
2022-08-08 11:45:45 -0700

    created general flow structure for logic

--- index.js
+++ index.js
@@ -1,4 +1,58 @@
 // Importing necessary packages
-const inquirer = require('inquirer');
+const Inquirer = require('inquirer');
 const cTable = require('console.table');
 const database = require('./config/connection');
+
+// Questions to prompt user upon starting the application
+const options = ["View All Departments", "View All Roles", "View All Employees", "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role", "Delete a Department", "Delete a Role", "Delete an Employee", "QUIT"];
+
+// Main menu
+const mainMenu = [{
+    type: "list",
+    message: "What would you like to do?",
+    choices: options,
+    name: menuChoice
+}];
+
+async function init() {
+    let selectedOption = await Inquirer.prompt(mainMenu);
+    let continuePrompt = true;
+
+    while (continuePrompt) {
+        switch (selectedOption) {
+            case "View All Departments":
+                // function to view all departments;
+                return;
+            case "View All Roles":
+                // function to view all roles;
+                return;
+            case "View All Employees":
+                // function to view all employees
+                return;
+            case "Add a Department":
+                // function to add a department
+                return;
+            case "Add a Role":
+                // function to add a role
+                return;
+            case "Add an Employee":
+                // function to add an employee
+                return;
+            case "Update an Employee Role":
+                // function to update an employee role
+                return;
+            case "Delete a Department":
+                // function to delete a department
+                return;
+            case "Delete a Role":
+                // function to delete a role
+                return;
+            case "Delete an Employee":
+                // function to delete a employee
+                return;
+            case "QUIT":
+                continuePrompt = false;
+                return;
+        }
+    }
+}
