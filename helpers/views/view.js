c3a056278523c5f43631c73179ee83e12ef0f18d
Felix Cagatin <cagatingilbert@gmail.com>
2022-08-08 14:34:33 -0700

    Fixed view all employees query

--- helpers/views/view.js
+++ helpers/views/view.js
@@ -17,8 +17,22 @@ function viewRole() {
 }
 
 // function to view ALL roles
+
+/* WHEN I choose to view all employees
+ * THEN I am presented with a formatted table showing employee data, including:
+ *      employee ids, first names, last names, 
+ *      job titles, departments, salaries, and managers that the employees report to.
+*/
+let empQuery = `
+SELECT emp.id, emp.first_name AS First, emp.last_name AS Last, r.title AS Title, dep.name AS Department, r.salary AS Salary, emp.manager_id AS Manager
+FROM employee as emp
+LEFT JOIN role AS r
+ON emp.role_id = r.id
+LEFT JOIN department AS dep
+ON r.department_id = dep.id
+`
 function viewEmployee() {
-    database.query('SELECT * FROM employee', (err, res) => {
+    database.query(empQuery, (err, res) => {
         err ? console.log("Error retrieving Employee Table", err) : console.table('\n', res);
     });
 }
