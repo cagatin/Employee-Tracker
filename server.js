6a5a2f711ff9b2f2ccbb277d5754976f55270fb4
Felix Cagatin <cagatingilbert@gmail.com>
2022-08-08 11:25:54 -0700

    Removed server packages

--- server.js
+++ server.js
@@ -1,19 +1,4 @@
 // Importing necessary packages
-const express = require('express');
+const inquirer = require('inquirer');
 const cTable = require('console.table');
 const database = require('./config/connection');
-
-// Create instance of express.
-const app = express();
-
-// Port
-const PORT = process.env.PORT || 3001;
-
-// middlewear for body parsing
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
-
-// Cr5eate server
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
