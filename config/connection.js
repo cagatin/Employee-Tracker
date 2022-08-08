813f41d383635dd7c4420625879506f2f11d4e98
Felix Cagatin <cagatingilbert@gmail.com>
2022-08-08 10:19:49 -0700

    Added query for using company_db

--- config/connection.js
+++ config/connection.js
@@ -12,3 +12,14 @@ const database = mysql.createConnection(
         password: process.env.PASSWORD
     }
 )
+
+// Create the company_db database if it doesnt exist.
+const createDBString = 'CREATE DATABASE IF NOT EXISTS company_db';
+database.query(createDBString, (err, res) => {
+    err ? console.log('Error in creating company_db', err) : console.log('company_db Database Created!')
+});
+
+// Use the company_db database
+database.query('USE company_db', (err, res) => {
+    err ? console.log('Error in using company_db', err) : console.log('Using company_db Database');
+});
