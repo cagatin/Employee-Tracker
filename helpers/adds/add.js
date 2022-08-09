f50cff6ffa19017902e5d9c0010ef7d9780d3487
Felix Cagatin <cagatingilbert@gmail.com>
2022-08-09 11:48:39 -0700

    Added check to see if department value is unique

--- helpers/adds/add.js
+++ helpers/adds/add.js
@@ -22,16 +22,25 @@ const addDeptQuestions = [{
 // Function to add a department
 async function addDepartment() {
     const deptToAddData = await Inquirer.prompt(addDeptQuestions);                         // prompt user what department to add.
-    const deptToAdd = capitalize(deptToAddData.deptName);                                 // extract the department name from the prompt and format the string
+    const deptToAdd = [capitalize(deptToAddData.deptName)];                                 // extract the department name from the prompt and format the string
 
-    let deptQuery = `INSERT INTO department (name) VALUES (?);`
-    database.query(deptQuery, deptToAdd, (err) => {
-        if (err) {
-            console.log('Issue in creating department', err);
+    //determine if the department is already in the database
+    let searchDept = `SELECT count(*) FROM department WHERE name = '?'`
+    database.execute(searchDept, deptToAdd, (err, res) => {
+        if (res.length === 1) {
+            console.log("\n Department is already in the Database!");
+            return;
+        } else {
+            let deptQuery = `INSERT INTO department (name) VALUES (?);`
+            database.execute(deptQuery, deptToAdd, (err) => {
+                if (err) {
+                    console.log('\n Issue in creating department', err);
+                }
+            });
+
+            return;
         }
     });
-
-    return;
 }
 
 
