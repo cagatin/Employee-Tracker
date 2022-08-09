6b64ccbd8c0a71de01e465fd3548e06548148b88
Felix Cagatin <cagatingilbert@gmail.com>
2022-08-09 10:27:43 -0700

    Fixed department view query

--- helpers/views/view.js
+++ helpers/views/view.js
@@ -3,16 +3,18 @@ const cTable = require('console.table');
 const database = require('../../config/connection');
 
 // function to view ALL deparments
+let deptQuery = `
+SELECT dep.id as ID, dep.name as Name
+FROM department as dep
+`
 function viewDepartment() {
-    database.query('SELECT * FROM department', (err, res) => {
+    database.query(deptQuery, (err, res) => {
         err ? console.log("Error retrieving Department Table", err) : console.table('\n', res);
     });
     return;
 }
 
 // function to view ALL roles
-/*THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-*/
 let roleQuery = `
 SELECT r.id as ID, r.title as Title, dep.name as Department, r.salary as Salary
 FROM role as r
@@ -27,12 +29,6 @@ function viewRole() {
 }
 
 // function to view ALL roles
-
-/* WHEN I choose to view all employees
- * THEN I am presented with a formatted table showing employee data, including:
- *      employee ids, first names, last names, 
- *      job titles, departments, salaries, and managers that the employees report to.
-*/
 let empQuery = `
 SELECT emp.id, emp.first_name AS First, emp.last_name AS Last, r.title AS Title, dep.name AS Department, r.salary AS Salary, emp.manager_id AS Manager
 FROM employee as emp
