27e847f9f90fbd364ad1497098ac2dcbdf65267f
Felix Cagatin <cagatingilbert@gmail.com>
2022-08-09 11:05:52 -0700

    added a capitalize function to format user input

--- helpers/adds/add.js
+++ helpers/adds/add.js
@@ -4,6 +4,15 @@ const database = require('../../config/connection');
 const Inquirer = require('inquirer');
 const { default: inquirer } = require('inquirer');
 
+// Function to capitalize the first letter in the user input
+function capitalize(str) {
+    let arrStr = str.split(" ");
+    for (let i = 0; i < arrStr.length; i++) {
+        arrStr[i] = arrStr[i].charAt(0).toUpperCase() + arrStr[i].slice(1);
+    }
+    return arrStr.join(" ");
+}
+
 const addDeptQuestions = [{
     type: "input",
     message: "What is the name of the department?",
@@ -12,8 +21,8 @@ const addDeptQuestions = [{
 
 // Function to add a department
 async function addDepartment() {
-    const deptToAddData = await Inquirer.prompt(addDeptQuestions);      // prompt user what department to add.
-    const deptToAdd = deptToAddData.deptName;                                 //extract the department name from the prompt
+    const deptToAddData = await Inquirer.prompt(addDeptQuestions);                         // prompt user what department to add.
+    const deptToAdd = capitalize(deptToAddData.deptName);                                 // extract the department name from the prompt and format the string
 
     let deptQuery = `INSERT INTO department (name) VALUES (?);`
     database.query(deptQuery, deptToAdd, (err) => {
@@ -21,6 +30,7 @@ async function addDepartment() {
             console.log('Issue in creating department', err);
         }
     });
+
     return;
 }
 
